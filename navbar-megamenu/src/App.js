6db68f82445dfbc8375de9d2ef8f97e6cd73ea65
Kennedy Mutua <mutuakennedy81@gmail.com>
2023-01-05 06:25:48 +0000

    add sitenav drop down

--- navbar-megamenu/src/App.js
+++ navbar-megamenu/src/App.js
@@ -13,6 +13,17 @@ const Navbar = () => {
             <ul>
               <li>
                 <button type="button">Products</button>
+                <div className="dropdown">
+                  <div className="dropdown-wrapper">
+                    <span>Products</span>
+                    <ul>
+                      <li><iconify-icon icon="logos:salesforce"></iconify-icon>Salesforce</li>
+                      <li><iconify-icon icon="logos:slack-icon"></iconify-icon>Slack</li>
+                      <li><iconify-icon icon="logos:jira"></iconify-icon>Jira</li>
+                      <li><iconify-icon icon="vscode-icons:file-type-reactjs"></iconify-icon>React</li>
+                    </ul>
+                  </div>
+                </div>
               </li>
               <li>
                 <button type="button">Industries</button>
