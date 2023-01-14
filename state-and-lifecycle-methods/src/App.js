0891175e0a1b7a2b8cafb17316ef19c7db4fd159
Kennedy Mutua <mutuakennedy81@gmail.com>
2023-01-14 20:21:56 +0000

    add constructor method, componenDidMount

--- state-and-lifecycle-methods/src/App.js
+++ state-and-lifecycle-methods/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  // The constructor method is called first when the component initializes
+  // It initialize the local state of a component
+  // Allow us to bind our compoent event handlers so we can modify state
+  constructor(props) {
+    // super method allow us to acces this.props in out component
+    super(props);
+    this.state = {
+      counter: 1,
+    };
+
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+  }
+
+  increment = () => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+  decrement = () => {
+    this.setState({ counter: this.state.counter - 1 });
+  };
+
+  // Called after render/when component is added to the dom
+  // Usefull to do things that require DOM access
+  componentDidMount() {
+    console.log("Component mounted");
+  }
+
+  render() {
+    const increment = this.increment;
+    const decrement = this.decrement;
+
+    return (
+      <>
+        <span>{this.state.counter}</span>
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+      </>
+    );
+  }
 }
 
 export default App;
